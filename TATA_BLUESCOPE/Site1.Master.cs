b585334999b76f4e0901ff97da552c697bdefe09
Abhijeet Kumar Pandit <72613084+itsabhijeetid@users.noreply.github.com>
2022-07-28 13:45:41 +0530

    Add files via upload

---
+++ TATA_BLUESCOPE/Site1.Master.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TATA_BLUESCOPE
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            { 
+                if (Session["role"]==null)
+                {
+                    logout.Visible = false;
+                    adminlogin.Visible = true;
+                    logo_linkbtn.Visible = true;
+                }
+                else if (Session["role"] == "user")
+                {
+                    logout.Visible = true;
+                    adminlogin.Visible = true;
+                    logo_linkbtn.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void logout_Click(object sender, EventArgs e)
+        {
+            Session["role"] = null;
+            logout.Visible = false;
+            adminlogin.Visible = true;
+            logo_linkbtn.Visible = true;
+            Response.Redirect("login.aspx");
+        }
+
+        protected void adminlogin_Click(object sender, EventArgs e)
+        {
+            Session["role"] = "admin";
+            logout.Visible = false;
+            adminlogin.Visible = false;
+            logo_linkbtn.Visible = true;
+            Response.Redirect("admin.aspx");
+        }
+    }
+}
---
+++ TATA_BLUESCOPE/login.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace TATA_BLUESCOPE
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string strcon = "server = localhost; user id = root; password = root; persist security info = true; database = tata_bluescope";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Login button 
+        protected void btn_Login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Session["role"] = null;
+                MySqlConnection con = new MySqlConnection(strcon);
+                if (con.State != ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                MySqlCommand cmd = new MySqlCommand("select * from userlogin where userid = '"+ txt_Username.Text.Trim()+ "' AND password = '"+ txt_password.Text+ "'",con);
+                MySqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                     while (dr.Read())
+                     {
+                        Response.Write("<script> alert('Login Successful'); </script>");
+                        Session["role"] = "user";
+                    }
+                    Response.Redirect("shiftdata.aspx");
+                    
+                }
+                else
+                {
+                    Response.Write("<script> alert('Wrong User ID or Password'); </script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert('" + ex.Message + "'); </script>");
+            }
+        }
+    }
+}
